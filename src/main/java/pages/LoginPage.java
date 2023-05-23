a910f979eb87e18af8990f9da5bb311f5b7b1326
Bondulya <98536601+Bondulya@users.noreply.github.com>
2023-05-23 18:25:56 +0300

    Add files via upload

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import Data.LoginData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    private final String URL_LOGIN_PAGE = "https://stellarburgers.nomoreparties.site/login";
+
+    private final By INPUT_EMAIL_FIELD = By.xpath(".//fieldset/div/div/input");
+    private final By INPUT_PASSWORD_FIELD = By.xpath(".//input[@name='Пароль']");
+    private final By SUBMIT_LOGIN_BUTTON = By.xpath(".//button");
+
+    private final By ENTER_HEADER = By.xpath("//h2");
+
+    public void LoadingPage() {
+        checkLocator(ENTER_HEADER);
+    }
+
+    public void getLoginPage() {
+        getPage(URL_LOGIN_PAGE);
+    }
+
+    public void fillLoginForm(LoginData loginData) {
+        type(INPUT_EMAIL_FIELD, loginData.getEmail());
+        type(INPUT_PASSWORD_FIELD, loginData.getPassword());
+    }
+
+    public void submitLogin() {
+        click(SUBMIT_LOGIN_BUTTON);
+    }
+
+    public void loginByUser(LoginData loginData) {
+        fillLoginForm(loginData);
+        submitLogin();
+    }
+
+}
---
+++ src/test/java/RegistrationTests.java
@@ -0,0 +1,29 @@
+import Data.RegistrationData;
+import org.junit.Before;
+import org.junit.Test;
+
+import static ToDo.Randomize.*;
+
+public class RegistrationTests  extends TestBase {
+
+    @Override
+    @Before
+    public void setUp() {
+        init("RegistrationPage");
+    }
+    @Test
+    public void positiveRegistrationTest() {
+        registrationPage.createUser(new RegistrationData(getRandomName(),
+                getRandomEmail(),
+                getRandomPassword(8)));
+        loginPage.LoadingPage();
+    }
+
+    @Test
+    public void negativeRegistrationTest() {
+        registrationPage.createUser(new RegistrationData(getRandomName(),
+                getRandomEmail(),
+                getRandomPassword(5)));
+        registrationPage.checkWrongPasswordMessage();
+    }
+}
